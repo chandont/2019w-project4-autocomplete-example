7982291b33de147b3950fcaff531825822f79122
zuozhiw <zuozhiw@gmail.com>
2018-02-26 17:15:52 -0800

    add more features, add comments

--- WebContent/index.js
+++ WebContent/index.js
@@ -1,6 +1,17 @@
+
+/*
+ * This function is called by the library when it needs to lookup a query.
+ * 
+ * The parameter query is the query string.
+ * The doneCallback is a callback function provided by the library, after you get the
+ *   suggestion list from AJAX, you need to call this function to let the library know.
+ */
 function handleLookup(query, doneCallback) {
 	console.log("autocomplete initiated")
 	console.log("sending AJAX request to backend Java Servlet")
+	
+	// if you want to check past query results first
+	// you can do it here
 	
 	// sending the HTTP GET request to the Java Servlet endpoint hero-suggestion
 	// with the query data
@@ -8,6 +19,7 @@ function handleLookup(query, doneCallback) {
 		"method": "GET",
 		"url": "hero-suggestion?query=" + escape(query),
 		"success": function(data) {
+			// pass the data, query, and doneCallback function into the success handler
 			handleLookupAjaxSuccess(data, query, doneCallback) 
 		},
 		"error": function(errorData) {
@@ -17,6 +29,12 @@ function handleLookup(query, doneCallback) {
 	})
 }
 
+/*
+ * This function is used to handle the ajax success callback function.
+ * 
+ * data is the JSON data string you get from the Java Servlet
+ * 
+ */
 function handleLookupAjaxSuccess(data, query, doneCallback) {
 	console.log("lookup ajax successful")
 	
@@ -28,20 +46,52 @@ function handleLookupAjaxSuccess(data, query, doneCallback) {
 	// you can do it here
 
 	// call the callback function provided by the autocomplete library
-	doneCallback({suggestions: jsonData});
+	// add "{suggestions: jsonData}" to satisfy the library response format
+	// see "Response Format" section in documentation
+	doneCallback( { suggestions: jsonData } );
 }
 
+/*
+ * This function is the select suggestion hanlder function. 
+ * When a suggestion is selected, this function is called by the library.
+ * 
+ * You can redirect to the page you want using the suggestion data.
+ */
+function handleSelectSuggestion(suggestion) {
+	console.log("you select " + suggestion["value"])
+	var url = suggestion["data"]["category"] + "-hero" + "?id=" + suggestion["data"]["heroID"]
+	
+	console.log(url)
+}
 
-
+// bind autocomplete library
 $('#autocomplete').autocomplete({
 	// see the documentation https://github.com/devbridge/jQuery-Autocomplete
 	// about how the lookup function can used under the "Custom lookup function" section
     lookup: function (query, doneCallback) {
     		handleLookup(query, doneCallback)
     },
+    onSelect: function(suggestion) {
+    		handleSelectSuggestion(suggestion)
+    },
     // set the groupby name in the response json data field
     groupBy: "category",
     // set delay time
-    deferRequestBy: 500
+    deferRequestBy: 300,
+    // there are some other parameters that you might want to use to satisfy all the requirements
 });
+
+
+function handleNormalSearch(query) {
+	console.log("doing normal search with query: " + query);
+}
+
+// bind pressing enter key
+$('#autocomplete').keypress(function(event) {
+	// keyCode 13 is the enter key
+	if (event.keyCode == 13) {
+		// pass the value of the input box to the hanlder function
+		handleNormalSearch($('#autocomplete').val())
+	}
+})
 
--- src/HeroSuggestion.java
+++ src/HeroSuggestion.java
@@ -1,9 +1,6 @@
 
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,96 +11,135 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
-/**
- * Servlet implementation class MovieSuggestion
- */
+// server endpoint URL
 @WebServlet("/hero-suggestion")
 public class HeroSuggestion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-	public static List<String> marvelHeros = Arrays.asList(
-			"Blade",
-			"Ghost Rider",
-			"Luke Cage",
-			"Silver Surfer",
-			"Beast",
-			"Thing",
-			"Black Panther",
-			"Invisible Woman",
-			"Nick Fury",
-			"Storm",
-			"Iron Man",
-			"Professor X",
-			"Hulk",
-			"Cyclops",
-			"Thor",
-			"Jean Grey",
-			"Wolverine",
-			"Daredevil",
-			"Captain America",
-			"Spider-Man");
+	/*
+	 * populate the Marvel heros and DC heros map.
+	 * Key is hero ID. Value is hero name.
+	 */
+	public static HashMap<Integer, String> marvelHerosMap = new HashMap<>();
+	public static HashMap<Integer, String> dcHerosMap = new HashMap<>();
 	
-	public static List<String> dcHeros = Arrays.asList(
-			"Superman",
-			"Batman",
-			"Wonder Woman",
-			"Flash",
-			"Green Lantern",
-			"Catwoman",
-			"Nightwing",
-			"Captain Marvel",
-			"Aquaman",
-			"Green Arrow",
-			"Martian Manhunter",
-			"Batgirl",
-			"Supergirl",
-			"Black Canary",
-			"Hawkgirl",
-			"Cyborg",
-			"Robin");
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+	static {
+		marvelHerosMap.put(1, "Blade");
+		marvelHerosMap.put(2, "Ghost Rider");
+		marvelHerosMap.put(3, "Luke Cage");
+		marvelHerosMap.put(4, "Silver Surfer");
+		marvelHerosMap.put(5, "Beast");
+		marvelHerosMap.put(6, "Thing");
+		marvelHerosMap.put(7, "Black Panther");
+		marvelHerosMap.put(8, "Invisible Woman");
+		marvelHerosMap.put(9, "Nick Fury");
+		marvelHerosMap.put(10, "Storm");
+		marvelHerosMap.put(11, "Iron Man");
+		marvelHerosMap.put(12, "Professor X");
+		marvelHerosMap.put(13, "Hulk");
+		marvelHerosMap.put(14, "Cyclops");
+		marvelHerosMap.put(15, "Thor");
+		marvelHerosMap.put(16, "Jean Grey");
+		marvelHerosMap.put(17, "Wolverine");
+		marvelHerosMap.put(18, "Daredevil");
+		marvelHerosMap.put(19, "Captain America");
+		marvelHerosMap.put(20, "Spider-Man");
+	}
+	
+	static {
+		dcHerosMap.put(101, "Superman");
+		dcHerosMap.put(102, "Batman");
+		dcHerosMap.put(103, "Wonder Woman");
+		dcHerosMap.put(104, "Flash");
+		dcHerosMap.put(105, "Green Lantern");
+		dcHerosMap.put(106, "Catwoman");
+		dcHerosMap.put(107, "Nightwing");
+		dcHerosMap.put(108, "Captain Marvel");
+		dcHerosMap.put(109, "Aquaman");
+		dcHerosMap.put(110, "Green Arrow");
+		dcHerosMap.put(111, "Martian Manhunter");
+		dcHerosMap.put(112, "Batgirl");
+		dcHerosMap.put(113, "Supergirl");
+		dcHerosMap.put(114, "Black Canary");
+		dcHerosMap.put(115, "Hawkgirl");
+		dcHerosMap.put(116, "Cyborg");
+		dcHerosMap.put(117, "Robin");
+	}
+    
     public HeroSuggestion() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    /*
+     * 
+     * Match the query against Marvel and DC heros and return a JSON response.
+     * 
+     * For example, if the query is "super":
+     * The JSON response look like this:
+     * [
+     * 	{ "value": "Superman", "data": { "category": "dc", "heroID": 101 } },
+     * 	{ "value": "Supergirl", "data": { "category": "dc", "heroID": 113 } }
+     * ]
+     * 
+     * The format is like this because it can be directly used by the 
+     *   JSON auto complete library this example is using. So that you don't have to convert the format.
+     *   
+     * The response contains a list of suggestions.
+     * In each suggestion object, the "value" is the item string shown in the dropdown list,
+     *   the "data" object can contain any additional information.
+     * 
+     * 
+     * You can also design different JSON formats to return to the frontend Javascript.
+     * For example, you want to use a different library and that library uses another format.
+     * Designing a format independent of the frontend library can make
+     *   your API not restricted by the library you use.
+     * 
+     * [
+     *  { 
+     *  	  "category": "dc",
+     *    "suggestions": [ 
+     *      { "name": "Superman", "heroID": 101 }, 
+     *      { "name": "Supergirl", "heroID": 113 } 
+     *     ]
+     *  },
+     *  { 
+     *    "category: "marvel",
+     *    "suggestions": [ ]
+     *  }
+     * ]
+     * 
+     * 
+     */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		try {
+			// setup the response json arrray
 			JsonArray jsonArray = new JsonArray();
 			
+			// get the query string from parameter
 			String query = request.getParameter("query");
 			
+			// return the empty json array if query is null or empty
 			if (query == null || query.trim().isEmpty()) {
 				response.getWriter().write(jsonArray.toString());
 				return;
 			}	
-						
-			List<String> matchedMarvelHeros = new ArrayList<>();
-			for (String s : marvelHeros) {
-				if (s.toLowerCase().contains(query.toLowerCase())) {
-					matchedMarvelHeros.add(s);
+			
+			// search on marvel heros and DC heros and add the results to JSON Array
+			// this example only does a substring match
+			// in project 4, you should do full text search with MySQL to find the matches on movies and stars
+			
+			for (Integer id : marvelHerosMap.keySet()) {
+				String heroName = marvelHerosMap.get(id);
+				if (heroName.toLowerCase().contains(query.toLowerCase())) {
+					jsonArray.add(generateJsonObject(id, heroName, "marvel"));
 				}
 			}
 			
-			List<String> matchedDcHeros = new ArrayList<>();
-			for (String s : dcHeros) {
-				if (s.toLowerCase().contains(query.toLowerCase())) {
-					matchedDcHeros.add(s);
+			for (Integer id : dcHerosMap.keySet()) {
+				String heroName = dcHerosMap.get(id);
+				if (heroName.toLowerCase().contains(query.toLowerCase())) {
+					jsonArray.add(generateJsonObject(id, heroName, "dc"));
 				}
-			}
-			
-			for (String s : matchedMarvelHeros) {
-				jsonArray.add(generateJsonObject(s, "marvel"));
-			}
-			for (String s: matchedDcHeros) {
-				jsonArray.add(generateJsonObject(s, "dc"));
 			}
 			
 			response.getWriter().write(jsonArray.toString());
@@ -114,12 +150,23 @@ public class HeroSuggestion extends HttpServlet {
 		}
 	}
 	
-	private static JsonObject generateJsonObject(String hero, String category) {
+	/*
+	 * Generate the JSON Object from hero and category to be like this format:
+	 * {
+	 *   "value": "Iron Man",
+	 *   "data": { "category": "marvel", "heroID": 11 }
+	 * }
+	 * 
+	 */
+	private static JsonObject generateJsonObject(Integer heroID, String heroName, String categoryName) {
 		JsonObject jsonObject = new JsonObject();
-		jsonObject.addProperty("value", hero);
-		JsonObject categoryJsonObject = new JsonObject();
-		categoryJsonObject.addProperty("category", category);
-		jsonObject.add("data", categoryJsonObject);
+		jsonObject.addProperty("value", heroName);
+		
+		JsonObject additionalDataJsonObject = new JsonObject();
+		additionalDataJsonObject.addProperty("category", categoryName);
+		additionalDataJsonObject.addProperty("heroID", heroID);
+		
+		jsonObject.add("data", additionalDataJsonObject);
 		return jsonObject;
 	}
 
